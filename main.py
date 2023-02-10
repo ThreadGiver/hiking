375a26298a3bd2536a69a66946e63052f20c2486
None <axlevert@gmail.com>
2023-02-10 10:22:30 -0500

    converted to object oriented programming

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
 import rasterio
+from rasterio.windows import Window
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-dataset = rasterio.open('data/30N000E_20101117_gmted_mea300.tif').read()[0]
+class Idk():
+    def __init__(self, file_name, remove_zeroes=False) -> None:
+        self.file = rasterio.open(f'data/{file_name}')
+        self.dataset = self.file.read(1)
+        self.height = self.dataset.shape[0]
+        self.width = self.dataset.shape[1]
 
-height = dataset.shape[0]
-width = dataset.shape[1]
+        if remove_zeroes:
+            self.dataset = np.ma.masked_where(self.dataset == 0, self.dataset)
 
-def crop(ymin, ymax, xmin, xmax):
-    dataset = dataset[ymin:height-ymax, xmin:width-xmax]
-    height -= ymin + ymax
-    width -= xmin + xmax
 
-def remove_zeroes():
-    dataset = np.ma.masked_where(dataset == 0, dataset)
+    def crop(self, col_off: int, row_off: int, width: int, height: int):
+        """Crops the dataset
 
-# nice cmap with norm log: gist_earth gray terrain
-plt.imshow(dataset, cmap='gist_earth', norm='log')
+        Args:
+            col_off (int): column offset
+            row_off (int): row offset
+            width (int): crop to width
+            height (int): crop to height
+        """
+        self.dataset = self.file.read(1, window=Window(col_off, row_off, width, height))
+    
+    def imshow(self, cmap: int | str = 0, norm='log'):
+        """Calls the matplotlib imshow method with preset variables.
+
+        Args:
+            cmap (int | str, optional): plt.imshow cmap var. Defaults to 0.
+            norm (str, optional): plt.imshow norm var. Defaults to 'log'.
+        """
+        if isinstance(cmap, int):
+            cmap = ['gist_earth', 'gray', 'terrain'][cmap]
+        
+        plt.imshow(self.dataset, cmap=cmap, norm=norm)
+
+
+italy = Idk('30N000E_20101117_gmted_mea300.tif')
+
+italy.imshow()
 
 plt.show()
