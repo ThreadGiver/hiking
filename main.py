e7bfe78b2b1be4df6919b4a34613eae944aa6850
TheAxan <axlevert@gmail.com>
2023-03-03 11:48:56 -0500

    added new maps

--- main.py
+++ main.py
@@ -35,46 +35,47 @@ class Mapper():
         ))
         return self
     
-    def imshow_dataset(self, cmap = 'gist_earth', norm = 'log'):
+    def imshow_dataset(self, cmap = 'gist_earth', norm = 'log', vmin = None):
         """Calls the matplotlib imshow method with preset variables.
 
         Args:
             cmap (int | str, optional): plt.imshow cmap variable. 
                 An int input selects a recommended cmap. Defaults to 'gist_earth'.
             norm (str, optional): plt.imshow norm variable. Defaults to 'log'.
+
         """
         if isinstance(cmap, int):
             cmap = ['gist_earth', 'gray', 'terrain'][cmap]
-        plt.imshow(self.dataset, cmap=cmap, norm=norm)
+        plt.imshow(self.dataset, cmap=cmap, norm=norm, vmin=vmin)
             
-    def imshow_gradient(self, cmap = 'seismic', norm = 'linear'):
-        plt.imshow(np.gradient(self.dataset)[0], cmap=cmap, norm=norm)
+    def imshow_gradient(self, cmap = 'seismic', norm = 'linear', vmin = None):
+        plt.imshow(np.gradient(self.dataset)[0], cmap=cmap, norm=norm, vmin=vmin)
     
 x = 1000
 y = 1000
-italy = Mapper('30N000E_20101117_gmted_mea300.tif')
+select_map = Mapper('gc_dem.tif')
 
-path = pathing.a_star((1400,1900), (960,370), italy.dataset, (0, ))
+path = pathing.a_star((1400,1900), (960,370), select_map.dataset, (0, ))
 
 pathTransposed = np.array(path).T.tolist()
 
-# italy2 = Mapper('30N000E_20101117_gmted_mea300.tif')
+# select_map2 = Mapper('30N000E_20101117_gmted_mea300.tif')
 
-# idk = np.full(italy2.dataset.shape, np.nan)
-# italy2.dataset[:] = 0
-# italy2.dataset[0] = np.nan
-# italy2.dataset[tuple(pathTransposed)] = 10000
+# idk = np.full(select_map2.dataset.shape, np.nan)
+# select_map2.dataset[:] = 0
+# select_map2.dataset[0] = np.nan
+# select_map2.dataset[tuple(pathTransposed)] = 10000
 
-idk = np.zeros(italy.dataset.shape)
+idk = np.zeros(select_map.dataset.shape)
 idk[tuple(pathTransposed)] = 1
 
-italy.imshow_dataset()
+select_map.imshow_dataset()
 
 
 # c_white = mpl.colors.colorConverter.to_rgba('yellow',alpha = 0.01)
 # c_black= mpl.colors.colorConverter.to_rgba('red',alpha = 1)
 # cmap_rb = mpl.colors.LinearSegmentedColormap.from_list('rb_cmap',[c_white,c_black],2)
 
-plt.imshow(idk, 'hsv', alpha=0.5)
+# plt.imshow(idk, 'hsv', alpha=0.5)
 
 plt.show()
