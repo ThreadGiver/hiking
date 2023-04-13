b2ac10e0fa1cf6a29ae9b9ecb0d96a016026429c
TheAxan <axlevert@gmail.com>
2023-04-13 12:51:46 -0400

    further testing of computation options

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ def gradient_to_time(array, dx=800):
     output = (dx/1000) / output # /1000 if dx with dx in m, if you don't divide it takes wayyy longer
     return output
 
-select_map = Mapper('italy300', remove_zeroes=False)
+
+end_coords = (600, 300)
+select_map = Mapper('italy300', remove_zeroes=True)
 
 # print(select_map.dataset)
 # print()
@@ -36,7 +38,7 @@ dataset = gradient_to_time(np.gradient(select_map.dataset)[0])
 time1 = time.time()
 print(time1-time0)
 
-path = pathing.time_a_star((600,300), (960,370), dataset)
+path = pathing.time_a_star(end_coords, (960,370), dataset)
 path_transposed = np.array(path).T.tolist()
 plt.plot(path_transposed[1], path_transposed[0], c='r')
 time2 = time.time()
@@ -46,12 +48,27 @@ plt.show()
 
 select_map = Mapper('italy300')
 select_map.imshow_dataset()
-dataset = np.gradient(select_map.dataset)[0]
 
 time3 = time.time()
-path = pathing.gradient_a_star((600,300), (960,370), dataset)
-path_transposed = np.array(path).T.tolist()
-plt.plot(path_transposed[1], path_transposed[0], c='r')
+dataset = gradient_to_time(np.gradient(select_map.dataset)[0])
 time4 = time.time()
 print(time4-time3)
+
+
+path = pathing.time_a_star(end_coords, (960,370), dataset)
+path_transposed = np.array(path).T.tolist()
+plt.plot(path_transposed[1], path_transposed[0], c='r')
+time5 = time.time()
+print(time5-time4)
+
+select_map = Mapper('italy300')
+select_map.imshow_dataset()
+dataset = np.gradient(select_map.dataset)[0]
+
+time6 = time.time()
+path = pathing.gradient_a_star(end_coords, (960,370), dataset)
+path_transposed = np.array(path).T.tolist()
+plt.plot(path_transposed[1], path_transposed[0], c='r')
+time7 = time.time()
+print(time7-time6)
 plt.show()
