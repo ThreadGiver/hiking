7285cde5da028b1454a50fc8ef728e0e50f8562e
TheAxan <axlevert@gmail.com>
2023-05-19 18:13:48 -0400

    touch up

--- main.py
+++ main.py
@@ -17,22 +17,22 @@ italy075_coords = [
 
 canyon_coords = [
     (3680, 1670), # start
-    (3320, 2240),
     (3030, 2560), # end
+    (3320, 2240),
     (2500, 3900),
     (3200, 5100),
     (3600, 2000)
 ]
 
-start_coords = italy300_coords[0]
-end_coords = italy300_coords[1]
+start_coords = italy075_coords[0]
+end_coords = italy075_coords[1]
 
-select_map = Mapper('italy300')
+select_map = Mapper('italy075')
 select_map.imshow_dataset()
 
 dataset = np.gradient(select_map.dataset)
 
-path = pathing.a_star(start_coords, end_coords, dataset, h_factor=100)
+path = pathing.a_star(start_coords, end_coords, dataset, h_factor=10, dx=200)
 path_transposed = np.array(path).T.tolist()
 plt.plot(path_transposed[1], path_transposed[0], c='r')
 
